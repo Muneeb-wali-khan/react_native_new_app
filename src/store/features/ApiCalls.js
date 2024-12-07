a580cb9035eb79eafe1a41e9b2a487c436624a61
MuneebWaliKhan09 <muneebwali809@gmail.com>
2024-12-07 18:46:46 +0500

    c

--- src/store/features/ApiCalls.js
+++ src/store/features/ApiCalls.js
@@ -1,12 +1,12 @@
 import {fetchBaseQuery, createApi} from '@reduxjs/toolkit/query/react';
 import {API_URL} from '../../strings/strings';
 import Config from 'react-native-config';
-const WETHER_API_KEY = Config.WETHER_API_KEY;
+const NEWS_API_KEY = Config.NEWS_API_KEY;
 
 const endpoints = {
   // All articles mentioning Apple from yesterday, sorted by popular publishers first
-  apple: `everything?q=apple&from=2024-11-06&to=2024-11-06&sortBy=popularity&apiKey=${WETHER_API_KEY}`,
-  tesla: `everything?q=tesla&from=2024-10-07&sortBy=publishedAt&apiKey=${WETHER_API_KEY}`,
+  apple: `everything?q=apple&from=2024-11-06&to=2024-11-06&sortBy=popularity&apiKey=${NEWS_API_KEY}`,
+  tesla: `everything?q=tesla&from=2024-10-07&sortBy=publishedAt&apiKey=${NEWS_API_KEY}`,
 };
 
 export const NewsAPI = createApi({
